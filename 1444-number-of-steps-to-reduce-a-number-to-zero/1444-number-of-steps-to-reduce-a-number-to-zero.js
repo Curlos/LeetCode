d1aba69a40dc734c52acc7875c78c4e7dcd95375
Carlos Martinez <41396365+curlos@users.noreply.github.com>
2024-09-06 07:23:49 -0400

    Time: 55 ms (46.11%), Space: 48.5 MB (89.15%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.js
@@ -6,11 +6,17 @@ var numberOfSteps = function(num) {
     let numOfSteps = 0
     let numClone = num
 
-    while (numClone !== 0) {
+    while (numClone > 0) {
         const isEven = numClone % 2 === 0
-        isEven ? numClone /= 2 : numClone -= 1
+
+        if (isEven) {
+            numClone /= 2
+        } else {
+            numClone -= 1
+        }
+
         numOfSteps++
     }
-    
+
     return numOfSteps
 };
