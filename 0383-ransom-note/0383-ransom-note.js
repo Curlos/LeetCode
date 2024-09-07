837701d4e3ecf50653690180d96d0c093d187f64
Carlos Martinez <41396365+curlos@users.noreply.github.com>
2024-09-07 11:05:22 -0400

    Time: 115 ms (5.41%), Space: 58.1 MB (5.09%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,26 +4,27 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    // Go through "magazine" and create a hashmap of all the characters and the number of times they appear in "magazine". For example, if you have "aaab", then the hashmap will be {"a": 3, "b": 1}.
-    const magazineChars = {}
+    // Sort both "ransomNote" and "magazine"
+    const ransomNoteArr = ransomNote.split('').sort()
+    const magazineArr = magazine.split('').sort()
 
-    for (let letter of magazine) {
-        if (!magazineChars[letter]) {
-            magazineChars[letter] = 0
-        }
+    // Setup two pointers: one for "ransomNote" and one for "magazine"
+    let ransomNotePointer = 0
+    let magazinePointer = 0
 
-        magazineChars[letter] += 1
-    }
+    // Using the two pointers go through the letters in ransom note in the sorted order of letters
 
-    // Go through all the characters in ransom note and check if that letter exists in the hashmap and that the count is greater than 0.
-    for (let letter of ransomNote) {
-        if (magazineChars[letter]) {
-            magazineChars[letter] -= 1
+    // "aa"    "aab"
+    while (ransomNotePointer < ransomNoteArr.length && magazinePointer < magazineArr.length) {
+        if (ransomNoteArr[ransomNotePointer] === magazineArr[magazinePointer]) {
+            ransomNotePointer++
+            magazinePointer++
         } else {
-            return false
+            magazinePointer++
         }
+        
     }
 
-    // If we go through all of the characters in "magazine" without returning false, then this means that we had enough letters and so we can return true.
-    return true
+    const foundAllLetters = ransomNotePointer >= ransomNoteArr.length ? true : false
+    return foundAllLetters
 };
